a6df4e9ab1738ff41a544c2fd4d8ac60ce007b73
caaiiors <caiorissasilveiraa@gmail.com>
2025-12-26 23:51:48 -0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,22 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import terser from '@rollup/plugin-terser'
+import terser from "@rollup/plugin-terser";
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    minify: 'terser',
-    terserOptions: {
+  plugins: [
+    react(),
+    terser({
       compress: {
         drop_console: true,
+        drop_debugger: true,
       },
       mangle: true,
-    },
-  },
+      format: {
+        comments: false,
+      },
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
