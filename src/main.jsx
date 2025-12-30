3de22ec4360d255a318ae4d9165772f18d8d8ae1
caaiiors <caiorissasilveiraa@gmail.com>
2025-12-30 15:56:57 -0300

    Adicionar suporte ao React Router: configurar rotas para a página de destino do Facebook e reescrever a lógica de renderização

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import FacebookLandingPage from "./screens/FacebookLandingPage";
 import PersistentBackground from "@/components/PersistentBackground";
 import "./components/index.css";
 import { ToastProvider } from "./components/Toast";
-import { PremiumProvider } from "./context/PremiumContext.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router>
-    <ToastProvider>
-      <PersistentBackground />
-      <Routes>
-        <Route path="/facebook" element={<FacebookLandingPage />} />
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </ToastProvider>
-  </Router>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Renderiza baseado na rota
+if (window.location.pathname === "/facebook") {
+  root.render(
+    <React.StrictMode>
+      <ToastProvider>
+        <FacebookLandingPage />
+      </ToastProvider>
+    </React.StrictMode>
+  );
+} else {
+  root.render(
+    <React.StrictMode>
+      <ToastProvider>
+        <PersistentBackground />
+        <App />
+      </ToastProvider>
+    </React.StrictMode>
+  );
+}
